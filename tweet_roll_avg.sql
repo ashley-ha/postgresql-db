768da821f9f72662895acc97701dec9a82b4d0f7
Ashley Eastman <66715836+ashley-ha@users.noreply.github.com>
2023-06-27 12:23:01 +0900

    updated tweet_roll_avg.sql

---
+++ tweet_roll_avg.sql
@@ -0,0 +1,16 @@
+-- calculate the 3-day rolling average of tweets per user
+-- output: user_id, tweet_date, rolling avgs to 2 decimals
+-- running mean
+
+select user_id, tweet_date, round(rolling_avg, 2) as rolling_avg_3d
+from (
+select 
+  user_id, 
+  tweet_date, 
+    avg(tweet_count) OVER(
+    PARTITION BY user_id
+    order by tweet_date
+    rows between 2 preceding and current row
+  ) as rolling_avg
+  
+  from tweets
